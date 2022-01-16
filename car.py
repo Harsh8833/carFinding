df053961f219d3281e72553b780af0c6d1cacc27
keshav8833 <keshavdalmia8833@gmail.com>
2022-01-16 12:15:00 +0530

    choice update

--- car.py
+++ car.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
+<<<<<<< HEAD
 data = pd.read_csv('Cardata.csv')
+=======
+data = pd.read_csv('Projects\carFinding\Cardata.csv')
+>>>>>>> 4ccd1328f00effc7831fef0d066247826d7124dd
 
 def headerlist():
     listOfHeader = list()
     for i in data:
         listOfHeader.append(i)
     return listOfHeader
-
+ 
 currentIndex = "Car_Id"
 data.set_index(currentIndex, inplace=True)
 info = data.loc[1]
